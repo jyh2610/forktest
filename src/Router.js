89dad7d95e9f7bfe65f859dbc5648fdf1d8225f3
조윤환 <114235846+jyh2610@users.noreply.github.com>
2023-02-14 11:00:46 +0900

    test2

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
-import Article from "./Article";
+
 import Articles from "./Articles";
 
 export default function Router() {
